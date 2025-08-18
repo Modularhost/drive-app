6be9bf1f947c2423586003a498bbc74e9709c6ef
Modularhost <host.modular@gmail.com>
2025-08-18 09:23:46 -0400

    Update cargar.js

--- module/completar/cargar/cargar.js
+++ module/completar/cargar/cargar.js
@@ -4,13 +4,13 @@ import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/
 import * as XLSX from 'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/xlsx.mjs';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDmAf-vi7PhzzQkPZh89q9p3Mz4vGGPtd0",
-    authDomain: "modular-app-387da.firebaseapp.com",
-    projectId: "modular-app-387da",
-    storageBucket: "modular-app-387da.firebasestorage.app",
-    messagingSenderId: "271561966774",
-    appId: "1:271561966774:web:e197c00e2abd67b4f0d217",
-    measurementId: "G-7YT6MMR47X"
+  apiKey: "AIzaSyDuF7p0X6N8IE19Bqt78LQAp805tMl84Ds",
+  authDomain: "modular-app-16bd6.firebaseapp.com",
+  projectId: "modular-app-16bd6",
+  storageBucket: "modular-app-16bd6.firebasestorage.app",
+  messagingSenderId: "1006327040835",
+  appId: "1:1006327040835:web:b8b4f510da46514a3d3df6",
+  measurementId: "G-GVKBWL9GT9"
 };
 
 try {
@@ -1354,4 +1354,5 @@ try {
 } catch (error) {
     console.error('Error en la inicialización:', error.message);
     alert('Ocurrió un error al inicializar la aplicación.');
+
 }
