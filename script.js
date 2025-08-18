fa99307f3a9a69816cb72395dc037cadf9bc55bf
Modularhost <host.modular@gmail.com>
2025-08-18 09:21:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/fir
 import { getFirestore, collection, query, where, getDocs, doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmAf-vi7PhzzQkPZh89q9p3Mz4vGGPtd0",
-  authDomain: "modular-app-387da.firebaseapp.com",
-  projectId: "modular-app-387da",
-  storageBucket: "modular-app-387da.firebasestorage.app",
-  messagingSenderId: "271561966774",
-  appId: "1:271561966774:web:e197c00e2abd67b4f0d217",
-  measurementId: "G-7YT6MMR47X"
+  apiKey: "AIzaSyDuF7p0X6N8IE19Bqt78LQAp805tMl84Ds",
+  authDomain: "modular-app-16bd6.firebaseapp.com",
+  projectId: "modular-app-16bd6",
+  storageBucket: "modular-app-16bd6.firebasestorage.app",
+  messagingSenderId: "1006327040835",
+  appId: "1:1006327040835:web:b8b4f510da46514a3d3df6",
+  measurementId: "G-GVKBWL9GT9"
 };
 
 let app;
@@ -598,4 +598,5 @@ loginForm.addEventListener('submit', async (e) => {
     modalLoginButton.classList.remove('loading');
     modalLoginButton.textContent = 'Ingresar';
   }
+
 });
